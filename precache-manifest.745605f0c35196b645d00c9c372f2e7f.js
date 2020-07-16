ce11dbc483d7b66c973da2c885b84c463e27a14a
MayankPrasad99 <mayankpr.99@gmail.com>
2020-07-16 22:45:49 +0530

    Updates

---
+++ precache-manifest.745605f0c35196b645d00c9c372f2e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99660d33ede7eaf59f810fa480be63d5",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "8c8ed806ba4328c9f14a",
+    "url": "/covid_tracker/static/css/main.91e79a36.chunk.css"
+  },
+  {
+    "revision": "19132bca68de2162cb04",
+    "url": "/covid_tracker/static/js/2.32223a27.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.32223a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8ed806ba4328c9f14a",
+    "url": "/covid_tracker/static/js/main.004f36f9.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
